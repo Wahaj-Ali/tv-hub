9bb3eb326efdfd710dc4805fa41691f8631055ec
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-03 17:46:43 +0500

    fetched data from API and displayed it on Home Page
    in this commit
    - fetched list of shows from API and displayed their data on the home page.
    - added comments and reservations buttons.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './style.css';
+
+class Shows {
+    constructor(){
+        this.API_URL = 'https://api.tvmaze.com/';
+        this.shows = [];
+    }
+
+    getShows = async(showName) => {
+        const Query = `search/shows?q=${showName}`;
+        const list = await fetch(`${this.API_URL}${Query}`).then((response) => response.json());
+        this.shows = [...list];
+    }
+
+    displayShows = async () => {
+        await this.getShows(6);
+
+        const showsList = this.shows.reduce((prev, curr) => {
+            if(curr.show.image) {
+             prev += `
+             <div class="shows">
+             
+               <div class="show-image">
+                <img src=${curr.show.image.medium} />
+               </div>
+               <div class="show-name">
+                 <span>${curr.show.name}</span>
+                 <div class="like"> 
+                 <i class="fa fa-heart fa-lg" data-pos=${curr.show.id}></i> 
+                </div>
+               </div>
+               <div class="comments">
+                 <button class="btn" id="${curr.show.id}">Comments</button>
+                 <button class="btn" id="${curr.show.id}">Reservations</button>
+               </div>
+         
+           </div>
+           `;
+            }
+            return prev;
+         }, '');
+
+        document.querySelector('.showslist').innerHTML = showsList;
+    }
+}
+
+const series = new Shows();
+series.displayShows();
