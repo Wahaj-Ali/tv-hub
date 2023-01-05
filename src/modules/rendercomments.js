07e21d5aa1e3050158f712510405b9d6218ac32d
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-05 18:03:49 +0200

    Create rendercomments.js to POST and FETCH

---
+++ src/modules/rendercomments.js
@@ -0,0 +1,43 @@
+const commUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rhcywbVKX5F63o89jgrY/comments';
+
+export const renderComment = async (id, name, comment) => {
+  const response = await fetch(`${commUrl}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: id, username: name, comment }),
+  });
+  return response;
+};
+
+const fetchUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rhcywbVKX5F63o89jgrY/comments';
+
+const fetchComment = async (id) => {
+  const reponseComment = await fetch(`${fetchUrl}?item_id=${id}`);
+  const itemData = await reponseComment.json();
+  return itemData;
+};
+
+/* eslint-disable */
+const createComments = ({ comment, username, creation_date }) => {
+  const commentli = document.createElement('li');
+  commentli.className = 'comment-li';
+  commentli.innerHTML = `
+   <span>${creation_date}: </span><span>${username}: </span><span>${comment}</span>
+  `;
+  return commentli;
+};
+/* eslint-enable */
+export const generateComment = async (id) => {
+  const commentUl = document.createElement('ul');
+  commentUl.id = 'comments-section';
+  commentUl.innerHTML = '';
+  const comments = await fetchComment(id);
+
+  comments.forEach((comment) => {
+    commentUl.append(createComments(comment));
+  });
+
+  return commentUl;
+};
