76ff73f6740182f6616e11e52ea2f51033f6addf
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-05 20:41:59 +0500

    Update showscount.test.js
    removed linter errors

--- src/tests/showscount.test.js
+++ src/tests/showscount.test.js
@@ -1,20 +1,19 @@
 import { itemCount } from "../modules/itemscounter.js";
 
 test('returns number of shows as 6', () => {
-    const shows = [{ item: 3 }, { item: 4 }, { item: 5 }, { item: 1 }, { item: 2 }, { item: 10 }];
-    const count = itemCount(shows);
-    expect(count).toBe(6);
-  });
-  
-  test('returns number of shows as 1', () => {
-    const shows = [{ item: 4 }];
-    const count = itemCount(shows);
-    expect(count).toBe(1);
-  });
-  
-  test('returns number of shows as 0', () => {
-    const shows = [];
-    const count = itemCount(shows);
-    expect(count).toBe(0);
-  });
+  const shows = [{ item: 3 }, { item: 4 }, { item: 5 }, { item: 1 }, { item: 2 }, { item: 10 }];
+  const count = itemCount(shows);
+  expect(count).toBe(6);
+});
 
+test('returns number of shows as 1', () => {
+  const shows = [{ item: 4 }];
+  const count = itemCount(shows);
+  expect(count).toBe(1);
+});
+
+test('returns number of shows as 0', () => {
+  const shows = [];
+  const count = itemCount(shows);
+  expect(count).toBe(0);
+});
