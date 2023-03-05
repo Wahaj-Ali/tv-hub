b2cb40eb30ddc64a27fe17eeddbc10ea0376f259
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-06 01:48:48 +0500

    Add loading animation
    In this commit
    - Add loading animation as the data is fetched from the API.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,47 @@ const searchInput = document.getElementById('searchInput');
 localStorage.setItem('object', JSON.stringify(userData));
 const dataOBJ = JSON.parse(localStorage.getItem('object'));
 
+const displayLoader = (displayed) => {
+  const main = document.querySelector('.showslist');
+  if (displayed) {
+    const loader = `
+    <div class="loader" title="7">
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         width="80px" height="80px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+        <rect x="0" y="10" width="4" height="10" fill="#fa0437" opacity="0.2">
+          <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+        </rect>
+        <rect x="8" y="10" width="4" height="10" fill="#fa0437"  opacity="0.2">
+          <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+        </rect>
+        <rect x="16" y="10" width="4" height="10" fill="#fa0437"  opacity="0.2">
+          <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+          <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+        </rect>
+      </svg>
+    </div>`;
+    main.innerHTML += loader;
+  } else {
+    main.querySelector('.loader')
+      .classList.add('hidden');
+  }
+};
+
 const series = new Shows();
-series.displayShows(dataOBJ.search);
+
+window.addEventListener('load', async () => {
+  displayLoader(true);
+  series.displayShows(dataOBJ.search);
 searchInput.addEventListener('change', () => {
   series.displayShows(searchInput.value);
 });
+displayLoader(false);
+})
+
+
+
